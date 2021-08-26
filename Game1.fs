5d08b5bff6ebc870d6ef4b460082eb186879740f
Matthew <matthewcrews@gmail.com>
2021-08-26 09:44:55 -0700

    map working

--- Game1.fs
+++ Game1.fs
@@ -3,6 +3,9 @@ namespace RobotMovementSim
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
+open MonoGame.Extended.Tiled
+open MonoGame.Extended.Tiled.Renderers
+
 open State
 
 
@@ -12,31 +15,25 @@ type Game1 () as this =
     let graphics = new GraphicsDeviceManager(this)
     let mutable spriteBatch = Unchecked.defaultof<_>
     let mutable robotSprite = Unchecked.defaultof<_>
-    let botCount = 1
     let mutable bots = Unchecked.defaultof<_>
+    let mutable tiledMap = nil<_>
+    let mutable tiledMapRenderer = nil<_>
 
     do
         this.Content.RootDirectory <- "Content"
         this.IsMouseVisible <- true
+
 
     override this.Initialize() =
         // TODO: Add your initialization logic here
         
         base.Initialize()
 
-    override this.LoadContent() =
-        spriteBatch <- new SpriteBatch(this.GraphicsDevice)
 
+
+    override this.LoadContent() =
         // TODO: use this.Content to load your game content here
         robotSprite <- this.Content.Load<Texture2D>("RobotTriangle")
-        // Create a single bot and move it to the right
-        //let circleBot1 = {
-        //    BotId = 1
-        //    Position = Vector2(0f, 0f)
-        //    Rotation = 0f
-        //    State = Stopped
-        //    Steps = [Right 100f; Down 100f; Left 100f; Up 100f]
-        //}
         let circleBot2 = {
             BotId = 2
             Position = Vector2(50f, 50f)
@@ -52,21 +49,32 @@ type Game1 () as this =
                 ]
         }
         bots <- [circleBot2]
+
+        tiledMap <- this.Content.Load<TiledMap>("samplemap")
+        tiledMapRenderer <- new TiledMapRenderer(this.GraphicsDevice, tiledMap)
+        spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+
+
  
     override this.Update (gameTime) =
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back = ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
         then this.Exit();
 
         // TODO: Add your update logic here
+        tiledMapRenderer.Update (gameTime)
         bots <- bots |> List.map (Bot.move gameTime)
 
         base.Update(gameTime)
  
+
+
     override this.Draw (gameTime) =
         this.GraphicsDevice.Clear Color.CornflowerBlue
 
         // TODO: Add your drawing code here
         spriteBatch.Begin()
+        
+        tiledMapRenderer.Draw()
 
         for bot in bots do
             //let sourceRect = Rectangle (0, 0, robotSprite.Width, robotSprite.Height)
